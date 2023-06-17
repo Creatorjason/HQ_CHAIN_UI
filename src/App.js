aeb6bdcf1d83ed2b3d7faf818e6cc2649087ce24
Creatorjason <107406861+Creatorjason@users.noreply.github.com>
2023-06-17 20:40:10 +0100

    feat: removed unused component in App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import CreateWallet from "./components/CreateWallet"
+
 import SendCoins from "./components/SendCoins"
 import './App.css';
 
@@ -9,15 +9,9 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      {/* <CreateWallet/> */}
+
       <SendCoins/>
-    {/* <Form
-    schema={schema}
-    validator={validator}
-    onChange={log('changed')}
-    onSubmit={log('submitted')}
-    onError={log('errors')}
-  /> */}
+
   </div>
   );
 }
